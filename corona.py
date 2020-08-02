86695f188998dc6084ae7476fd94b3a46939a02a
Ricepaste <58623697+Ricepaste@users.noreply.github.com>
2020-08-02 20:07:34 +0800

    tracker_0

--- corona.py
+++ corona.py
@@ -1,13 +1,7 @@
-# get covid-19 confirmed cases
-
 from selenium import webdriver  #安裝指令 pip install selenium
 import time
 
-user = 1
-if user == 0:
-    path = r'D:\Users\Administrator\Desktop\asfsaf\Python\collect_COVID-19\chromedriver.exe'
-else:
-    path = r'D:\實驗爬蟲\chromedriver.exe'
+path = r'D:\Users\Administrator\Desktop\asfsaf\Python\collect_COVID-19\chromedriver.exe'
 
 class Get_virus():
     def __init__(self): # initialization
@@ -18,20 +12,46 @@ class Get_virus():
             time.sleep(5) # wait for 5 sec to load the page
             self.get_data()
         except:
+            print('broken')
             self.driver.quit()
+
+            Get_virus()
+            
 
     def get_data(self):
         table = self.driver.find_element_by_xpath('//*[@id="nav-tabContent"]')
         tbody = table.find_element_by_xpath('//*[@id="main_table_countries_today"]/tbody[1]')
-        con_ids = tbody.find_element_by_xpath("//tr[contains(., 'Taiwan')]")
-        con = con_ids.find_elements_by_tag_name('td')
+        con_ids = tbody.find_elements_by_xpath('tr')
 
-        con_datas = []
+        con_datas = {}
+        temp_datas = []
 
-        for data in range(1, len(con)-1):
-            # print(data, con[data].text)
-            con_datas.append(con[data].text)
+        empty = False
+
+        for x in con_ids: # chek all ids
+            con = x.find_elements_by_tag_name('td') 
+            
+            empty = False
+
+            for data in range(2, len(con)-1):
+                if con[1].text == '': # check if it is empty
+                    print('empty')
+                    empty = True
+
+                    break
+                
+                temp_datas.append(con[data].text)
+
+            if not empty: # if the data is not empty, store it
+                print('get: ', con[1].text)           
+
+                con_datas[con[1].text] = temp_datas # con[1].text is country name
+
+            temp_datas = []
+
 
         print('found datas: ', con_datas)
+
+        print(con_datas['Taiwan'])
 
 Get_virus()
